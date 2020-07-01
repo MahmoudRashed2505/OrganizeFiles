d0bc3a558c9af2859542b7e9d252228fc0cdf192
Mahmoud Rashed <37276859+MahmoudRashed2505@users.noreply.github.com>
2020-07-01 03:08:07 +0200

    First Try

---
+++ organize.py
@@ -0,0 +1,11 @@
+from tkinter import *
+import time
+import os
+from os import system
+
+root = Tk(screenName="OrganizeFiles")
+myLabel = Label(root,text = "OrganizeFiles")
+myLabel.pack()
+root.mainloop()
+#path = askdirectory(title='Select Folder')
+#tkinter.Tk().withdraw()
