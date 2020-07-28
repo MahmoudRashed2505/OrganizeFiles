ebea01666fd1544d8afb46b3abc549fbe176039b
Mahmoud Rashed <37276859+MahmoudRashed2505@users.noreply.github.com>
2020-07-28 02:26:11 +0200

    First Step

--- organize.py
+++ organize.py
@@ -1,11 +1,36 @@
-from tkinter import *
 import time
 import os
-from os import system
+from tkinter import Button, Tk, filedialog, messagebox
+from tkinter.filedialog import askdirectory
+import shutil
 
-root = Tk(screenName="OrganizeFiles")
-myLabel = Label(root,text = "OrganizeFiles")
-myLabel.pack()
+#Creating a Function that Classify the Files
+def classify(file):
+    pass
+
+path = None
+#Creating Btn Function
+def getDirectory():
+    global path
+    path = askdirectory(title = 'Select Folder')
+    organize(path)
+
+#Create Function to Organize the files
+def organize(path):
+    os.chdir(path)
+    files = os.listdir()
+
+#Popup MSG Box after Complete organizing
+def popup():
+    messagebox.showinfo("Organizing Completed Successfuly !")
+
+#Creating new Form From Tkinter GUI
+root = Tk()
+root.title("Organize Your Files")
+#Creating The Main and only Btn
+organizeButton = Button(root, text="Choose a Folder To Organize !", padx=200,pady=200, command =getDirectory)
+organizeButton.pack()
+
 root.mainloop()
-#path = askdirectory(title='Select Folder')
-#tkinter.Tk().withdraw()
+
+
