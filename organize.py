ea9101b8dcb53d617316a6431719a03fcf3a2f05
Mahmoud Rashed <37276859+MahmoudRashed2505@users.noreply.github.com>
2020-07-30 23:23:28 +0200

    First Working Version

--- organize.py
+++ organize.py
@@ -5,8 +5,18 @@ from tkinter.filedialog import askdirectory
 import shutil
 
 #Creating a Function that Classify the Files
-def classify(file):
-    pass
+def classify(extention):
+    Videos = "avi mpg mpe mpeg asf wmv mov qt rm mp4 flv m4v webm ogv ogg mkv ts tsv".split()
+    Audio = "mp3 wav wma mpa ram ra aac aif m4a tsa".split()
+    Compressed = "zip rar r0* r1* arj gz sit sitx sea ace bz2 7z iso".split()
+    Software = "exe msi".split()
+    Documents = "doc pdf ppt pps docx pptx csv xlsx".split()
+    Photos = "JPG PNG TIF JPEG GIF TIFF PSD EPS AI".lower().split()
+    categories = {'Videos':Videos,'Audio':Audio,'Compressed':Compressed,"Software":Software,'Documents':Documents,'Photos':Photos}
+    for cat,ext in categories.items():
+        if extention in ext:
+            return cat
+    return "UnCategorized"
 
 path = None
 #Creating Btn Function
@@ -19,14 +29,51 @@ def getDirectory():
 def organize(path):
     os.chdir(path)
     files = os.listdir()
+    folders = list()
+    for file in files:
+        if os.path.isdir(file) == True:
+            folders.append(str(file))
+    checkFolders(folders)
+    categoryFolders = ['Videos','Music','Documents','Compressed','Software','UnCategorized']
+    for file in files:
+        if os.path.isfile(file) == True:
+            filename = str(file)
+            ext = filename.split('.')
+            ext = ext[-1]
+            cat = classify(ext)
+            shutil.move(file,os.path.join(os.getcwd(),cat))
+        if os.path.isdir(file) == True:
+            if str(file) not in categoryFolders:
+                shutil.move(file,os.path.join(os.getcwd(),'Folders'))
+    popup()
+
+
+def checkFolders(folders):
+    if 'Folders' not in folders:
+        os.mkdir('Folders')
+    if 'Videos' not in folders:
+        os.mkdir('Videos')
+    if 'Music' not in folders:
+        os.mkdir('Music')
+    if 'Documents' not in folders:
+        os.mkdir('Documents')
+    if 'Software' not in folders:
+        os.mkdir("Software")
+    if 'Compressed' not in folders:
+        os.mkdir('Compressed')
+    if 'UnCategorized' not in folders:
+        os.mkdir('UnCategorized')
+    if 'Photos' not in folders:
+        os.mkdir('Photos')
 
 #Popup MSG Box after Complete organizing
 def popup():
-    messagebox.showinfo("Organizing Completed Successfuly !")
+    messagebox.showinfo("Organizing Completed Successfuly !","Successfuly !")
 
 #Creating new Form From Tkinter GUI
 root = Tk()
 root.title("Organize Your Files")
+
 #Creating The Main and only Btn
 organizeButton = Button(root, text="Choose a Folder To Organize !", padx=200,pady=200, command =getDirectory)
 organizeButton.pack()
